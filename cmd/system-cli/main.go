22754c397592bf57992ae71444ab5cbae12e7233
Denis Arh <denis.arh@kendu.si>
2019-03-27 20:03:39 +0100

    Move cli setup & add settings cli IF

--- cmd/system-cli/main.go
+++ cmd/system-cli/main.go
@@ -4,8 +4,12 @@ import (
 	"log"
 	"os"
 
+	context "github.com/SentimensRG/ctx"
+	"github.com/SentimensRG/ctx/sigctx"
+
 	"github.com/crusttech/crust/internal/auth"
 	system "github.com/crusttech/crust/system"
+	"github.com/crusttech/crust/system/cli"
 )
 
 func main() {
@@ -13,9 +17,11 @@ func main() {
 	log.SetOutput(os.Stdout)
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 
+	ctx := context.AsContext(sigctx.New())
+
 	flags("system", system.Flags, auth.Flags)
 
+	// @todo no need to wake up entire System (migrations, external etc...)
 	system.Init()
-
-	setupCobra()
+	cli.Init(ctx)
 }
---
+++ system/cli/settings.go
@@ -0,0 +1,78 @@
+package cli
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+
+	"github.com/crusttech/crust/internal/settings"
+)
+
+func Settings(rootCmd *cobra.Command, service settings.Service) {
+	exit := func(err error) {
+		if err != nil {
+			rootCmd.Printf("Error: %v\n", err)
+			os.Exit(1)
+		} else {
+			os.Exit(0)
+		}
+	}
+
+	settingsCmd := &cobra.Command{
+		Use:   "settings",
+		Short: "Settings management",
+	}
+
+	list := &cobra.Command{
+		Use:   "list",
+		Short: "List all",
+		Run: func(cmd *cobra.Command, args []string) {
+			prefix := cmd.Flags().Lookup("prefix").Value.String()
+			if kv, err := service.FindByPrefix(prefix); err != nil {
+				exit(err)
+			} else {
+				for k, v := range kv {
+					cmd.Printf("%s\t%v\n", k, v)
+				}
+			}
+		},
+	}
+
+	list.Flags().String("prefix", "", "Filter settings by prefix")
+
+	get := &cobra.Command{
+		Use: "get [key to get]",
+
+		Short: "Get value (raw JSON) for a specific key",
+		Args:  cobra.ExactArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			if v, err := service.Get(args[0], 0); err != nil {
+				exit(err)
+			} else {
+				cmd.Printf("%v\n", v.Value)
+			}
+		},
+	}
+
+	settingsCmd.AddCommand(list, get)
+
+	settingsCmd.AddCommand(&cobra.Command{
+		Use:   "set [key to set] [value if not stdin]",
+		Short: "Set value (raw JSON) for a specific key",
+		Args:  cobra.MinimumNArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			value := args[1]
+			v := &settings.Value{
+				Name: args[0],
+			}
+
+			if err := v.SetValueAsString(value); err != nil {
+				exit(err)
+			}
+
+			exit(service.Set(v))
+		},
+	})
+
+	rootCmd.AddCommand(settingsCmd)
+}
